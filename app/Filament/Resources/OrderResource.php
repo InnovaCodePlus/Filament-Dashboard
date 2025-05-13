f8a2fe15b4440f958c22c5773589805732ffb8c6
Mattias Duarte <duartemattias3@gmail.com>
2025-05-12 21:56:25 -0400

    agregando vista de ordenes

---
+++ app/Filament/Resources/OrderResource.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OrderResource\Pages;
+use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Models\Order;
+use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrderResource extends Resource
+{
+    protected static ?string $model = Order::class;
+
+    protected static ?string $navigationGroup = 'CRM';
+    protected static ?string $navigationLabel = 'Salidas';
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
+    
+    protected static ?string $slug = "salidas";
+    protected static ?string $label = "Salida";
+    protected static ?string $pluralLabel = "Salidas";
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make("Informacion de facturación")
+                    ->schema([
+                        Forms\Components\Select::make('warehouse_id')
+                            ->relationship('warehouse', 'name')
+                            ->preload()
+                            ->searchable()
+                            ->label("Almacén")
+                            ->required(),
+
+                        Forms\Components\Select::make('customer_id')
+                            ->label("Cliente")
+                            ->preload()
+                            ->searchable()
+                            ->relationship('customer', 'name')
+                            ->required(),
+                    ]),
+
+                Forms\Components\TextInput::make('total')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('warehouse.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('customer.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrders::route('/'),
+            'create' => Pages\CreateOrder::route('/create'),
+            'edit' => Pages\EditOrder::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    protected $fillable = [
+        "user_id",
+        "customer_id",
+        "warehouse_id",
+        "total",
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function warehouse(): BelongsTo
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+
+
+}
