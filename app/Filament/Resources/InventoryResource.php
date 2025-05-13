9e269418e75fbd163432340dff6fa35d87f9e8d8
Mattias Duarte <duartemattias3@gmail.com>
2025-05-12 21:24:57 -0400

    agregando resource para la gestion de inventarios

---
+++ app/Filament/Resources/InventoryResource.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\InventoryResource\Pages;
+use App\Filament\Resources\InventoryResource\RelationManagers;
+use App\Models\Inventory;
+use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class InventoryResource extends Resource
+{
+    protected static ?string $model = Inventory::class;
+
+    
+    protected static ?string $navigationGroup = 'Menu principal';
+    protected static ?string $navigationLabel = 'Inventarios';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard';
+    
+    protected static ?string $slug = "inventario";
+    protected static ?string $label = "Inventario";
+    protected static ?string $pluralLabel = "Inventarios";
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Formulario de arqueo')
+                    ->columns(2)
+                    ->schema([
+                        Forms\Components\Select::make('product_id')
+                            ->relationship('product', 'name')
+                            ->label('Producto')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+                        Forms\Components\Select::make('warehouse_id')
+                            ->relationship('warehouse', 'name')
+                            ->searchable()
+                            ->label('Almacén')
+                            ->preload()
+                            ->required(),
+                        Forms\Components\TextInput::make('stock')
+                            ->required()
+                            ->numeric()
+                            ->default(0),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('product.name')
+                    ->numeric()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('warehouse.name')
+                    ->numeric()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('stock')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('warehouse_id')
+                    ->label('Almacén')
+                    ->relationship('warehouse', 'name')
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListInventories::route('/'),
+            'create' => Pages\CreateInventory::route('/create'),
+            'edit' => Pages\EditInventory::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Product extends Model
 {
-    
+    protected static function booted()
+    {
+        static::creating(function ($product){
+            $product->slug = Str::slug($product->name);
+        });
+    }
+
     protected $fillable = [
         "name",
         "slug",
@@ -26,10 +33,9 @@ class Product extends Model
         return $this->belongsTo(Category::class);
     }
 
-    protected static function booted()
+    public function inventories(): HasMany
     {
-        static::creating(function ($product){
-            $product->slug = Str::slug($product->name);
-        });
+        return $this->hasMany(Inventory::class);
     }
+
 }
